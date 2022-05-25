a13a746ed3e9d58f04e28af0d1a63d2aa9b8cd46
Pupochek <alar.akilbekov99@gmail.com>
2022-05-25 23:45:21 +0600

    graph

--- assignment4/src/Vertex.java
+++ assignment4/src/Vertex.java
@@ -8,33 +8,33 @@ import java.util.LinkedList;
 
 public class Vertex<T> {
     private T data;
-    private final HashMap<T, Double> edges;
+    private final HashMap<T, Double> adjacents;
 
     public Vertex(T data){
         setData(data);
-        this.edges = new HashMap<>();
+        this.adjacents = new HashMap<>();
     }
 
     public void addAdjacentVertex(T dest, double weight){
-        edges.put(dest, weight);
+        adjacents.put(dest, weight);
     }
 
     @Nullable
     public Double getWeight(T v){
-        return edges.get(v);
+        return adjacents.get(v);
     }
 
     public boolean isAdjacent(T v){
-        return edges.containsKey(v);
+        return adjacents.containsKey(v);
     }
 
     public int getAdjacentVertexCount(){
-        return edges.size();
+        return adjacents.size();
     }
 
     public Iterable<T> getAdjacentVertices(){
         LinkedList<T> arr = new LinkedList<>();
-        for(T v: edges.keySet()){
+        for(T v: adjacents.keySet()){
             arr.addFirst(v);
         }
         return arr;
